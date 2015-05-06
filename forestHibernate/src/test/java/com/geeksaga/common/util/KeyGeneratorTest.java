a7120de890509de520279ec76562c3d8d12eb135
geeksaga <geeksaga@geeksaga.com>
2015-05-07 00:59:23 +0900

    중복되는 키 처리

--- forestHibernate/src/test/java/com/geeksaga/common/util/KeyGeneratorTest.java
+++ forestHibernate/src/test/java/com/geeksaga/common/util/KeyGeneratorTest.java
@@ -1,6 +1,11 @@
 package com.geeksaga.common.util;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -12,20 +17,51 @@ import org.junit.Test;
 public class KeyGeneratorTest
 {
     @Test
-    public void generatorKey()
+    public void testGeneratorKey()
     {
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
 
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
 
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
-        
+
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
-        
+
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
-        
+
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
-        
+
         assertNotSame(KeyGenerator.generateKey(), KeyGenerator.generateKey());
+    }
+
+    @Test
+    public void testUniqueKey()
+    {
+        int length = 10000;
+        Set<String> list = new HashSet<>(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            list.add(KeyGenerator.generateKey());
+        }
+
+        assertThat(list.size(), is(length));
+    }
+
+    @Test
+    public void testUniqueKeyUseSleep() throws InterruptedException
+    {
+        int length = 100;
+        
+        Set<String> list = new HashSet<>(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            list.add(KeyGenerator.generateKey());
+            
+            Thread.sleep(1);
+        }
+
+        assertThat(list.size(), is(length));
     }
 }
